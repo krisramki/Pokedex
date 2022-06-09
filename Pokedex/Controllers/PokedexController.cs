645a4a67e6e2de5d99b9693588a1bc951ee68793
Crazy Chris <krishramki@icloud.com>
2022-06-09 21:44:33 +0100

    Add project files.

---
+++ Pokedex/Controllers/PokedexController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Pokedex.Models;
+using System.Web.Http.Description;
+
+namespace Pokedex.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PokedexController : ControllerBase
+    {
+        private readonly ILogger<PokedexController> _logger;
+
+        public PokedexController(ILogger<PokedexController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("{pokemonName}")]
+        [ResponseType(typeof(PokemonResponseDto))]
+        public IActionResult GetBasicPokemonDetails(string pokemonName)
+        {
+            PokemonResponseDto response = new();
+            return Ok(response);
+        }
+
+
+        [HttpGet("translated/{pokemonName}")]
+        [ResponseType(typeof(PokemonResponseDto))]
+        public IActionResult GetPokemonWithTranslatedDescription(string pokemonName)
+        {
+            PokemonResponseDto response = new();
+            return Ok(response);
+        }
+    }
+}
